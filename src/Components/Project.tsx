c143cdb93f27c3a7fe206586c02792b950be239d
Thalis Trisch <thalis.trisch2003@gmail.com>
2023-06-12 17:24:13 -0300

    Add Netflix Clone

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -4,6 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import Criticando from "/PrintCriticando_1.png"
 import Pokedex from "/PrintPokedex_1.png"
 import Chat from "/PrintChat_2.png"
+import Netflix from "/PrintCloneNetflix.png"
 import Tech from "./Tech"
 
 import { useState } from "react";
@@ -53,7 +54,7 @@ function Skills(){
                         <a href="https://github.com/ThalisTrisch/FrontChat.git" target="_blanc"><button className="p-2 px-4 bg-zinc-300 my-4 rounded-xl"><AiFillGithub className="text-3xl"/></button></a>
                     </div>
                 </div>
-                <div className="flex flex-row w-full justify-center flex-wrap mb-8 gap-6 items-center">
+                <div className="flex flex-row w-full justify-center flex-wrap mb-10 gap-6 items-center">
                     <div className="w-80">
                         <img className="rounded-xl border-2 w-80 h-40" src={Pokedex} alt="project"/>
                         <div className="mt-4 flex items-center justify-center gap-4">
@@ -75,6 +76,30 @@ function Skills(){
                             <Tech tech="typescript"></Tech>
                             <Tech tech="PokéAPI"></Tech>
                         </div>
+                    </div>
+                </div>
+                <div className="flex flex-row w-full justify-center flex-wrap-reverse mb-12 gap-6 items-center ">
+                    <div className='w-2/4 h-fit bg-zinc-200 max-md:w-80 rounded-xl px-6 py-6'>
+                        <p className="text-2xl max-md:text-xl mb-2 text-justify max-md:text-center">
+                            <strong>Clone Netflix</strong>
+                        </p>
+                        <p className="text-md max-md:text-sm text-justify"> 
+                            Projeto criando o clone da interface da Netflix para o meu aperfeiçoamento em criação de layouts e paginas com melhor design. 
+                        </p>
+                        <div className="flex flex-row gap-2 mt-4 flex-wrap">
+                            <Tech tech="TaiwindCSS"></Tech>
+                            <Tech tech="ReactJS"></Tech>
+                            <Tech tech="typescript"></Tech>
+                        </div>
+                    </div>
+                    <div className="w-80">
+                        <img className="rounded-xl border-2 w-80 h-40" src={Netflix} alt="project"/>
+                        <div className="mt-4 flex items-center justify-center gap-4">
+                            <a href="https://github.com/ThalisTrisch/CloneNetflix" target="_blanc"><button className="p-2 px-4 bg-zinc-300 rounded-xl"><AiFillGithub className="text-3xl"/></button></a>
+                            <a href="https://clone-netflix-eight-ruddy.vercel.app/" target="_blanc">
+                                <button className=" text-xl text-white p-2 px-4 bg-green-400 rounded-xl border-green-400">acessar</button>
+                            </a> 
+                       </div>
                     </div>
                 </div>
             </div>
